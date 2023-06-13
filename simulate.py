62e1b64dae11d28e8c6d29863cf67349fe7b21d7
Duc Trung LE <leductrungxf@gmail.com>
2023-06-13 23:42:07 +0200

    Update

---
+++ simulate.py
@@ -0,0 +1,4 @@
+import time
+def simulate(*args, **kwargs):
+    time.sleep(5)
+    return 8., 2.
--- wrapper.py
+++ wrapper.py
@@ -11,6 +11,8 @@ class JcadNotification:
         self._callback = None
         yobjects = self._jcad_doc.ydoc.get_array("objects")
         yobjects.observe_deep(self._on_change)
+        doc.ydoc.observe_after_transaction(self.after_transaction)
+        self.target_name = None
 
     def connect(self, f: Callable):
         self._callback = f
@@ -23,13 +25,19 @@ class JcadNotification:
             if not isinstance(event, YMapEvent):
                 continue
             if "shapeMetadata" not in event.keys:
-                if self._callback is not None:
-                    passed, msg = self._callback()
-                    if not passed:
-                        target_name = event.target["name"]
-                        asyncio.create_task(self._add_warning(target_name, msg))
-                    else:
-                        asyncio.create_task(self._remove_warning())
+                self.target_name = event.target["name"]
+
+
+    def after_transaction(self, e):
+        if self._callback is not None and self.target_name is not None:
+            target_name = self.target_name
+            self.target_name = None
+            passed, msg = self._callback()
+            if not passed:
+                asyncio.create_task(self._add_warning(target_name, msg))
+            else:
+                asyncio.create_task(self._remove_warning())
+            
 
     async def _remove_warning(self):
         for old_name, old_annotation_id in self._highlighted_shape.items():
@@ -39,10 +47,11 @@ class JcadNotification:
         self._highlighted_shape = {}
 
     async def _add_warning(self, name, message):
-        # for old_name, old_annotation_id in self._highlighted_shape.items():
-        #     if old_name != name:
-        #         self._jcad_doc.set_color(old_name, None)
-        #     self._jcad_doc.remove_annotation(old_annotation_id)
+        for old_name, old_annotation_id in self._highlighted_shape.items():
+            if old_name != name:
+                self._jcad_doc.set_color(old_name, None)
+            self._jcad_doc.remove_annotation(old_annotation_id)
+        print('SET COLOR', name)
 
         self._jcad_doc.set_color(name, [1, 0, 0])
         annotation_id = self._jcad_doc.add_annotation(
